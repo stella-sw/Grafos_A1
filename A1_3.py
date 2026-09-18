0ff3413804a5ee536dbe0e4c70bd8da6ca5ae379
Amanda Da Silva De Paula <amanda.d.p2005@gmail.com>
2026-09-18 15:59:21 -0300

    correção na linhas: 99, na lógica do ciclo interno no parâmetro do insert

--- A1_3.py
+++ A1_3.py
@@ -6,20 +6,19 @@ arquivo = sys.argv[1]
 
 grafo = G_bib.Grafo(arquivo=arquivo)
 
+
 def hierholzer(grafo: G_bib.Grafo):
-    if (grafo.qtdArestas() == 0):
-        return (True, [])
-    
+    if grafo.qtdArestas() == 0:
+        return True, []
+
     num_vertices = grafo.qtdVertices()
 
-    C = [[0 for x in range (num_vertices + 1)] for x in range (num_vertices + 1)]
+    C = [[0 for x in range(num_vertices + 1)] for x in range(num_vertices + 1)]
 
     for u in range(1, num_vertices + 1):
         for v in range(1, num_vertices + 1):
             C[u][v] = len(grafo.Adj[u][v])
-            C[v][u] = len(grafo.Adj[v][u])
 
-    # Verifica se todos os graus são pares
     for u in range(1, num_vertices + 1):
         if grafo.grau(u) % 2 != 0:
             return False, None
@@ -31,7 +30,6 @@ def hierholzer(grafo: G_bib.Grafo):
             inicio = k
             break
 
-    # Verifica se todos os vértices com arestas pertencem à mesma componente
     visitados = [False] * (num_vertices + 1)
     fila = [inicio]
     visitados[inicio] = True
@@ -47,39 +45,42 @@ def hierholzer(grafo: G_bib.Grafo):
     for u in range(1, num_vertices + 1):
         if grafo.grau(u) > 0 and not visitados[u]:
             return False, None
-    
+
     r, Ciclo = buscarSubciclo(grafo, inicio, C)
 
-    if (r == False):
-        return (False, None)
-    
-    for u in range(1, num_vertices+1):
-        for v in range(1, num_vertices+1):
+    if r == False:
+        return False, None
+
+    for u in range(1, num_vertices + 1):
+        for v in range(1, num_vertices + 1):
             if C[u][v] > 0:
-                return(False, None)
-            
-    return(True, Ciclo)
+                return False, None
+
+    return True, Ciclo
+
 
 def buscarSubciclo(grafo: G_bib.Grafo, v, C):
     Ciclo = [v]
     t = v
 
-    while (True):
+    while True:
         u = None
+
         for w in grafo.vizinhos(v):
             if C[v][w] > 0:
                 u = w
                 break
-        if (u is None):
-            return (False, None)
-        
-        C[u][v] -= 1
+
+        if u is None:
+            return False, None
+
         C[v][u] -= 1
+        C[u][v] -= 1
 
         v = u
         Ciclo.append(v)
 
-        if (v == t):
+        if v == t:
             break
 
     Ciclo_original = Ciclo.copy()
@@ -87,23 +88,19 @@ def buscarSubciclo(grafo: G_bib.Grafo, v, C):
     for u in Ciclo_original:
         for w in grafo.vizinhos(u):
             if C[u][w] > 0:
-
-                r, Ciclo_interno = buscarSubciclo(
-                    grafo=grafo,
-                    v=u,
-                    C=C
-                )
+                r, Ciclo_interno = buscarSubciclo(grafo, u, C)
 
                 if r == False:
                     return False, None
 
-                if Ciclo_interno is not None:
-                    inserir = Ciclo.index(u)
+                posicao = Ciclo.index(u)
 
-                    for x in Ciclo_interno[1:]:
-                        Ciclo.insert(inserir, x)
-                        inserir += 1
-    return (True, Ciclo)
+                for x in Ciclo_interno[1:]:
+                    Ciclo.insert(posicao + 1, x)
+                    posicao += 1
+
+    return True, Ciclo
+
 
 resultado, ciclo = hierholzer(grafo)
 
